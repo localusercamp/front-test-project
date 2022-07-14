61efa728d5d4bc2667d3ae732c9b003b887e7547
Evgenia <evgenia.ugra@gmail.com>
2022-07-14 19:07:04 +0500

    изменен кастомный компонент

---
+++ src/components/UI/MyCheckIcon.vue
@@ -0,0 +1,30 @@
+<template>
+  <svg width="32" height="32" viewBox="0 0 32 32" :fill="currentColor" :class="{ 'active-icon': !disabled }">
+    <slot></slot>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'MyCheckIcon',
+  props: {
+    checked: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    currentColor() {
+      if(!this.disabled) {
+        return this.checked ? '#0E77BA' : '#BBCBD3';
+      } else {
+        return '#E1E7EB';
+      }
+    }
+  }
+}
+</script>
